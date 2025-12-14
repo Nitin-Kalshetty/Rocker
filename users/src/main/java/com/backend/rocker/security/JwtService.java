ddd4c34838ba85bc4b34c215270a76daa6083af9
Nitin-Kalshetty <nitinkalshetty3851@gmail.com>
2025-12-14 16:32:57 +0530

    added jwt secret key

---
+++ users/src/main/java/com/backend/rocker/security/JwtService.java
@@ -0,0 +1,33 @@
+package com.backend.rocker.security;
+
+import com.backend.rocker.model.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    @Value("${jwt.security.key}")
+    private String SECRET_KEY;
+
+    private SecretKey getSecretKey(){
+        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
+    }
+
+    String generateJwtAccessToken(User user){
+        return Jwts.builder()
+                .subject(user.getUsername())
+                .claim("phone",user.getPhoneNumber())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis()+ 1000*60*10))
+                .signWith(getSecretKey())
+                .compact();
+    }
+
+}

5f0087aded24c97ee01da00a9ad8ca4c7941bef5
Nitin-Kalshetty <nitinkalshetty3851@gmail.com>
2025-12-15 17:32:07 +0530

    added some jwt auth implementation

---
+++ users/src/main/java/com/backend/rocker/security/JwtFilter.java
@@ -0,0 +1,35 @@
+package com.backend.rocker.security;
+
+import com.backend.rocker.repository.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final UserRepository userRepository;
+    private final JwtService jwtService;
+
+    JwtFilter(UserRepository userRepository,JwtService jwtService){
+        this.userRepository = userRepository;
+        this.jwtService = jwtService;
+    }
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        final String requestTokenHeader = request.getHeader("Authorization");
+        if(requestTokenHeader == null || !requestTokenHeader.startsWith("Bearer")){
+            filterChain.doFilter(request,response);
+        }
+
+        String token = requestTokenHeader.split("Bearer ")[1];
+        String username = jwtService.
+    }
+}
